e333553429e90feddc7f839f8bcd098eb379010d
Victorious-Toluwalope <victorioustolu@gmail.com>
2022-06-24 16:54:25 +0100

    First try

---
+++ index.js
@@ -0,0 +1,24 @@
+const num1 = parseFloat(prompt('Enter first number:'));
+const num2 = parseFloat(prompt('Enter second number:'));
+
+const operator = prompt('Enter operator(+,-,*,/');
+let result = 0;
+
+if(operator == '+'){
+      result = num1 + num2;
+}
+else {
+      if(operator == '-'){
+            result = num1 - num2;
+      }
+      else if (operator == '*'){
+            result = num1 * num2;
+      }
+      else if (operator == '/'){
+            result = num1 / num2;
+      }
+alert(num1+operator+num2+'='+result)
+      }
+
+      
+
